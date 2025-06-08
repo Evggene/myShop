b28673af66d8cf2c929524272452f22ce7789df8
e.bezlepkin <e.bezlepkin@cdek.ru>
2025-06-08 18:06:40 +0700

    ошибка

--- src/test/java/org/bea/my_shop/TestMyShopApplication.java
+++ src/test/java/org/bea/my_shop/TestMyShopApplication.java
@@ -1,4 +1,4 @@
-package org.bea.my_shop.configuration4;
+package org.bea.my_shop;
 
 import org.bea.my_shop.MyShopApplication;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class TestMyShopApplication {
 
 }
---
+++ src/test/java/org/bea/my_shop/controller/RootControllerTest.java
@@ -0,0 +1,36 @@
+package org.bea.my_shop.controller;
+
+import org.bea.my_shop.TestMyShopApplication;
+import org.bea.my_shop.configuration4.HandlerConfiguration;
+import org.bea.my_shop.infrastructure.input.controller.RootController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RootController.class)
+public class RootControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void root_shouldRedirectToItemsPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/main/items"));
+    }
+}
